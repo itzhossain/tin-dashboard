51c1fe0dc6a01090c1871cf63d79a487220b545a
itzhossain <136744337+itzhossain@users.noreply.github.com>
2026-04-21 23:48:35 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Dashboard</title>
+</head>
+<body style="background:#111; color:white; font-family:sans-serif;">
+
+<h2>Dashboard</h2>
+
+<a href="tin_make.php">➡️ Go to TIN Make</a>
+
+</body>
+</html>
